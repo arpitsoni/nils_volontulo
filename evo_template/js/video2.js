7c7d5b07e69f5b6ae69debdfdf22e1ca46555bfd
Archit Soni <archit.cse@live.com>
2015-08-15 00:01:57 +0530

    adding template files , initial commit

---
+++ evo_template/js/video2.js
@@ -0,0 +1,15 @@
+"use strict";
+(function($) {
+	// initialize BigVideo
+    var BV = new $.BigVideo();
+	BV.init();
+	BV.show('vids/evolango.mp4',{ambient:true});
+	// BV.show('vids/evolango.webm',{ambient:true});
+	// BV.show('vids/evolango.ogv',{ambient:true});	
+})(jQuery);
+				
+
+
+
+
+
